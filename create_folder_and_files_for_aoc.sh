243d81b3c766711825270b617e923d3fc3bd9496
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-07 13:37:17 +0100

    copy same code for part 2

--- create_folder_and_files_for_aoc.sh
+++ create_folder_and_files_for_aoc.sh
@@ -51,3 +51,42 @@ def readInputfile(inputFile):
 if __name__ == "__main__":
     main()
 EOF
+
+cat << EOF > day$dayNumber"-part2.js"
+const fs = require('fs');
+
+// inputFile = 'day$dayNumber-input.txt';
+inputFile = 'day$dayNumber-test2.txt';
+
+solution();
+
+function solution() {
+    input = readInputfile(inputFile);
+   
+    console.log(input);
+}
+
+function readInputfile (inputFile) {
+    input = fs.readFileSync(inputFile, "utf-8").split("\n");
+}
+EOF
+
+cat << EOF > day$dayNumber"-part2.py"
+inputFile = "$inputfile";
+# inputFile = day$dayNumber"-test2.txt"
+input = []
+
+def main():
+    input = readInputfile(inputFile)    
+    print(input)    
+
+def readInputfile(inputFile):
+    with open(inputFile, "r") as file:
+        lines = file.readlines()
+        content_list = [line.strip() for line in lines]
+    return content_list
+
+if __name__ == "__main__":
+    main()
+EOF
+
