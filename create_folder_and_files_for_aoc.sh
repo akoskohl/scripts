abb88a3c4f3fa8039369221ae59555efb5591318
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-07 13:26:07 +0100

    new files

--- create_folder_and_files_for_aoc.sh
+++ create_folder_and_files_for_aoc.sh
@@ -8,3 +8,9 @@ foldername=day$dayNumber
 mkdir $foldername
 cd $foldername
 
+inputfile=day$dayNumber"-input.txt"
+
+touch day$dayNumber"-test1.txt"
+touch day$dayNumber"-test2.txt"
+touch $inputfile
+
