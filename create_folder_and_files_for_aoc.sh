27e3b14bbe2e407034b60f5b188b7f82344b9b00
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-07 13:26:52 +0100

    create js with readInputFile

--- create_folder_and_files_for_aoc.sh
+++ create_folder_and_files_for_aoc.sh
@@ -14,3 +14,21 @@ touch day$dayNumber"-test1.txt"
 touch day$dayNumber"-test2.txt"
 touch $inputfile
 
+cat << EOF > day$dayNumber"-part1.js"
+const fs = require('fs');
+
+// inputFile = 'day$dayNumber-input.txt';
+inputFile = 'day$dayNumber-test1.txt';
+
+solution();
+
+function solution() {
+    input = readInputfile(inputFile);
+   
+    console.log(input);
+}
+
+function readInputfile (inputFile) {
+    input = fs.readFileSync(inputFile, "utf-8").split("\n");
+}
+EOF
