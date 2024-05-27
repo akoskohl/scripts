eb0b91b20a7525f77101bcbee536d9640633217a
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-05-27 10:18:39 +0200

    mac app I use
    soon i will move machines and gathered all packages I use currently
    converted to brew install command
    to get the list brew deps --tree --installed

---
+++ mac_app_I_use_w_homebrew.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#install homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+set -v
+brew install --cask shortcat
+brew install --cask iterm2
+brew install --cask rectangle
+brew install --cask alt-tab
+brew install --cask visual-studio-code
+brew install --cask macs-fan-control
+brew install --cask nordvpn
+brew install --cask postman
+brew install --cask prusaslicer
+brew install --cask jupyterlab
+brew install --cask raspberry-pi-imager
+brew install docker
+brew install git
+brew install black
+brew install jmeter
+brew install kubernetes-cli
+brew install minikube
+brew install mypy
+brew install neovim
+brew install node
+brew install nvm
+brew install tfenv
+brew install tmux
+
+# Appstore:
+
+# Velja
