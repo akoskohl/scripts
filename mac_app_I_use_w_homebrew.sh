fc64c66734ea79442bb74f74482f5bb70bb4cfd6
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-10-02 14:46:14 +0200

    update

--- mac_app_I_use_w_homebrew.sh
+++ mac_app_I_use_w_homebrew.sh
@@ -1,43 +1,121 @@
 #!/bin/bash
 
-#install homebrew
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+#add path
+
+
+# follow progress in terminal
 set -v
 
+# install whatever you want
+
+#Formulae
+brew install bash
+brew install lazygit
+brew install openjdk
+brew install black
+brew install lazykube
+brew install openssl@3
+brew install brotli
+brew install libevent
+brew install pandoc
+brew install c-ares
+brew install libnghttp2
+brew install pcre2
+brew install ca-certificates
+brew install libpng
+brew install pipx
+brew install cairo
+brew install libsodium
+brew install pixman
+brew install certifi
+brew install libtiff
+brew install python-certifi
+brew install docker
+brew install libuv
+brew install python-lsp-server
+brew install docker-completion
+brew install libvterm
+brew install python@3.10
+brew install fontconfig
+brew install libx11
+brew install python@3.12
+brew install freetype
+brew install libxau
+brew install python@3.8
+brew install fzf
+brew install libxcb
+brew install python@3.9
+brew install gdbm
+brew install libxdmcp
+brew install readline
+brew install gettext
+brew install libxext
+brew install ripgrep
+brew install giflib
+brew install libxrender
+brew install sqlite
+brew install git
+brew install libyaml
+brew install tcl-tk
+brew install glib
+brew install little-cms2
+brew install terraform
+brew install graphite2
+brew install lpeg
+brew install tfenv
+brew install grep
+brew install luajit
+brew install tmux
+brew install harfbuzz
+brew install luv
+brew install tofuenv
+brew install helm
+brew install lz4
+brew install tree
+brew install httpie
+brew install lzo
+brew install tree-sitter
+brew install icu4c
+brew install minikube
+brew install unibilium
+brew install inkdrop-visualizer
+brew install mpdecimal
+brew install utf8proc
+brew install ipython
+brew install msgpack
+brew install xorgproto
+brew install jmeter
+brew install mypy
+brew install xz
+brew install jpeg-turbo
+brew install ncurses
+brew install youtube-dl
+brew install jq
+brew install neovim
+brew install yq
+brew install jupyterlab
+brew install node
+brew install zeromq
+brew install kubernetes-cli
+brew install nvm
+brew install zstd
+brew install lazydocker
+brew install oniguruma
+
+# Casks
+brew install --cask github
+brew install --cask mongodb-compass
 brew install --cask shortcat
 brew install --cask iterm2
-brew install --cask rectangle
-brew install --cask alt-tab
-brew install --cask visual-studio-code
-brew install --cask macs-fan-control
 brew install --cask nordvpn
-brew install --cask postman
-brew install --cask prusaslicer
+brew install --cask wkhtmltopdf
 brew install --cask jupyterlab
-brew install --cask raspberry-pi-imager
-brew install python@3.9
-brew install docker
-brew install git
-brew install lazygit
-brew install black
-brew install jmeter
-brew install kubernetes-cli
-brew install minikube
-brew install mypy
-brew install neovim
-brew install node
-brew install nvm
-brew install tfenv
-brew install helm
-brew install fzf
-brew install --cask mongodb-compass
-
-# brew install tmux
-# brew install bash
-# brew install tnk-studio/tools/lazykube
-# brew install python@3.10
-
+brew install --cask postman
+brew install --cask macs-fan-control
+brew install --cask prusaslicer
 
 # Appstore:
 # Velja
